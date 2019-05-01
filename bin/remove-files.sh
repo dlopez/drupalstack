146a96c85d1e0d874411e07e09cac0376960d52f
Daniel Lopez <dannylopez@protonmail.com>
2019-05-01 01:25:06 -0400

    Fix remove-files.sh script

--- bin/remove-files.sh
+++ bin/remove-files.sh
@@ -13,7 +13,6 @@ SCRIPT_PATH="$(cd ${SCRIPT_PATH} && pwd)"
 
 PROJECT_ROOT="$(cd ${SCRIPT_PATH}/.. && pwd)"
 
-sudo chmod 777 -R "${PROJECT_ROOT}/www"
 sudo rm -rf "${PROJECT_ROOT}/www"
 mkdir -p "${PROJECT_ROOT}/www/web"
 touch "${PROJECT_ROOT}/www/web/.gitkeep"
